bc11c2e5c1e3cff5da453aa85be61d9472495190
booolink <aeze716.james@gmail.com>
2019-06-28 13:32:20 +0800

    20190628b

--- js/main.js
+++ js/main.js
@@ -10,7 +10,6 @@ $(document).ready(function () {
         $('.container h1').fadeToggle(2000, function () {
             back();
         })
-
     }
     back();
 
@@ -18,29 +17,18 @@ $(document).ready(function () {
         $('.menu').toggleClass('menuani')
     })
 
-    // $(window).scroll(function () {
-    //     var srco = $(this).scrollTop()
-    //     function Top0() {
-    //         $('#box3').animate({ 'top': '0px' }, 2000);
-    //     }
-    //     if (srco >= 300) {
-    //         Top0();
-    //     }
-    // })
+    $(window).resize(function () {
 
-   
-        $(window).resize(function () {
+        var Docwidth = $('#box1').width()
 
-            var Docwidth = $('#box1').width()
+        var Docheight = $(document).height()
+        var box1height = $('#box1').height()
 
-            var Docheight = $(document).height()
-            var box1height = $('#box1').height()
+        $('.bgccolor').height(Docheight - box1height)
+        $('.bgccolor').width(Docwidth)
 
-            $('.bgccolor').height(Docheight - box1height)
-            $('.bgccolor').width(Docwidth)
-           
-        })
-  
+    })
+
 })
 
 
